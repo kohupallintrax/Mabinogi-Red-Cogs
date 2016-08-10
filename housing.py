1d95c390684363077303cbee243aafd8bfdf748e
Peter Atwater <peteratwater@gmail.com>
2016-08-09 23:28:20 -0700

    Update the output formatting. Add comments

--- housing.py
+++ housing.py
@@ -1,9 +1,5 @@
-# base URL for housing search 208.85.109.181/MabinogiShopAdv/ShopAdvertise.asp?Name_Server=mabius3&CharacterId=4503599627432536&Page=1&Row=7&SearchType=4&SortType=5&SortOption=1&SearchWord=
-# Require lxml
-
 import discord
 from discord.ext import commands
-import aiohttp
 import requests
 from xml.etree import ElementTree
 
@@ -16,23 +12,26 @@ class Housing:
     @commands.command()
     async def housing(self, search):
         """Search mabinogi housing"""
-        #Your code will go here
-        url = ("http://208.85.109.181/MabinogiShopAdv/ShopAdvertise.asp?Name_Server=mabius3&CharacterId=4503599627432536&Page=1&Row=7&SearchType=4&SortType=5&SortOption=1&SearchWord=" + search) #build the web adress
+        #Build the web address, hit the webpage then retrieve and parse the XML
+        url = ("http://208.85.109.181/MabinogiShopAdv/ShopAdvertise.asp?Name_Server=mabius3&CharacterId=4503599627432536&Page=1&Row=7&SearchType=4&SortType=5&SortOption=1&SearchWord=" + search) 
         response = requests.get(url)
         root = ElementTree.fromstring(response.content)
+		#Grab relevant attributes from the XML for Seller, Item and Price
         try:
             charNameList = []
             itemNameList = []
             itemPriceList = []
             housingList = []
             msg = ""
+			#Iterate through the XML file for each item hit and append it to the housing list. Price needs to be an integer
             for child in root.findall('ItemDesc'):
                 charName = child.get('Char_Name')
                 itemName = child.get('Item_Name')
                 itemPrice = int(child.get('Item_Price'))
                 housingList.append([charName, itemName, itemPrice])
+			#Construct the message our bot will say. Its ugly because I suck at programming. Would have done it the Python way but I wanted  commas in the price and didn't want to think ;)	
             for charName, itemName, itemPrice in housingList:
-                msg += "[Seller]: "+ charName + " [Item:] " + itemName + " [Price]: " + '{0:,}'.format(itemPrice) +  "\n\n"
+                msg += "[Seller]: "+ charName + " [Item]: " + itemName + " [Price]: " + '{0:,}'.format(itemPrice) +  "\n\n"
             await self.bot.say(msg)
         except:
             await self.bot.say("Couldn't search housing. No results or there was an error.")
